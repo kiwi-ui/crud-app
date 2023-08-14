43e39e890a07e761111af464e4f0d725b645a027
kiwi-ui <yusnianggara@gmail.com>
2023-08-14 22:28:55 +0700

    style: change style of navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,9 +3,9 @@ import {RiAccountCircleLine} from 'react-icons/ri';
 
 const Navbar = () => {
   return (
-    <nav className="navbar fixed-top bg-dark z10">
-        <div className="container my-2">
-            <a className="fs-5 mb-0 bg-warning bg-opacity-75 text-black px-2 rounded-2 fw-bold navbar-brand" href='_blank' rel='norefferer'>CrossOver</a>
+    <nav className="navbar bg-dark z10">
+        <div className="container">
+            <a className="mb-0 bg-warning bg-opacity-75 text-black px-2 rounded-2 fw-bold navbar-brand" href='_blank' rel='norefferer'>CrossOver</a>
 
             <div className="d-flex align-items-center gap-2" role="search">
                 <RiAccountCircleLine className="fs-1 text-white" />
