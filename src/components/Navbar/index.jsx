418c2a8ad164cddb9ce856c169da6a8aafcb6c8a
kiwi-ui <yusnianggara@gmail.com>
2023-08-15 09:15:02 +0700

    refactor: add header tag to navbar component

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,15 +3,17 @@ import {RiAccountCircleLine} from 'react-icons/ri';
 
 const Navbar = () => {
   return (
-    <nav className="navbar bg-dark z10">
-        <div className="container">
-            <a className="mb-0 bg-warning bg-opacity-75 text-black px-2 rounded-2 fw-bold navbar-brand" href='_blank' rel='norefferer'>CrossOver</a>
+    <header>
+        <nav className="navbar bg-dark z10">
+            <div className="container">
+                <a className="mb-0 bg-warning bg-opacity-75 text-black px-2 rounded-2 fw-bold navbar-brand" href='_blank' rel='norefferer'>CrossOver</a>
 
-            <div className="d-flex align-items-center gap-2" role="search">
-                <RiAccountCircleLine className="fs-1 text-white" />
+                <div className="d-flex align-items-center gap-2" role="search">
+                    <RiAccountCircleLine className="fs-1 text-white" />
+                </div>
             </div>
-        </div>
-    </nav>
+        </nav>
+  </header>
   )
 }
 
