0cb0f19304c3c4558c91f1492eab342db2779fc5
kiwi-ui <yusnianggara@gmail.com>
2023-08-15 09:16:06 +0700

    feat: add userdetail page

--- src/App.js
+++ src/App.js
@@ -2,25 +2,23 @@ import { useState } from 'react';
 import { BrowserRouter, Routes, Route, Outlet, Link, Navigate } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Loginpage from './pages/Loginpage';
+import UserDetails from './components/UserDetails';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState();
+  const [id, setId] = useState();
 
-  const handleLogin = () => {
-    // Simulate a successful login
-    setIsLoggedIn(true);
-  };
-
-  const handleLogout = () => {
-    setIsLoggedIn(false);
-  };
+  const handleLogin = () => { setIsLoggedIn(true) };
+  const handleLogout = () => { setIsLoggedIn(false) };
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Loginpage onLogin={handleLogin} />} />
-        <Route path="/dashboard" element={<Dashboard onLogout={handleLogout} />} />
-        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={ <Loginpage onLogin= { handleLogin } />} />
+        <Route path="/users" element={ <Dashboard onLogout= { handleLogout } setUser= { setUser } setId = {setId}  />} />
+        <Route path="/" element={ <Navigate to="/login" /> }/>
+        <Route path={ `/users/${id}` } element={ <UserDetails user={user} />}/>
       </Routes>
     </BrowserRouter>
   );
--- src/services/User/index.js
+++ src/services/User/index.js
@@ -4,3 +4,7 @@ import { environment } from "../../environment"
 export const User = () =>  {
     return axios.get(`${environment.apiUrl}/users`)
 };
+
+export const SingleUser = (id) =>  {
+    return axios.get(`${environment.apiUrl}/users/${id}`)
+};
