c1f0e85d5ef6798dee863b295ccc95bc6aa1e9ec
kiwi-ui <yusnianggara@gmail.com>
2023-08-15 15:55:19 +0700

    feat: add NotFound page and its condition

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,14 +1,9 @@
-import Navbar from "../../components/Navbar";
-
 const NotFound = () => {
   return (
-    <>
-        <Navbar />
-        <main className="vh-100 vw-100 d-flex justify-content-center align-items-center flex-column top-0">
-            <h2>404 - Page Not Found</h2>
-            <p>The requested page could not be found.</p>
-        </main>
-    </>
+    <main className="vh-100 vw-100 d-flex justify-content-center align-items-center flex-column">
+        <h2>404 - Page Not Found</h2>
+        <p>The requested page could not be found.</p>
+    </main>
   );
 };
 
