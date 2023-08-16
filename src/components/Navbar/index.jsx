4eee56d5c425bac115db02cc3b84f5ededeef278
kiwi-ui <yusnianggara@gmail.com>
2023-08-16 10:23:02 +0700

    style: change icon for logout

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link, redirect } from 'react-router-dom';
 import {FiLogOut} from 'react-icons/fi';
 
 const Navbar = () => {
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    redirect("/login")
+  }
+
   return (
     <header>
         <nav className="navbar z10 border-bottom py-1 bg-dark">
             <div className="container">
                 <Link className="mb-0 bg-warning text-black px-2 rounded-2 fw-bold navbar-brand" to="/users">CrossOver</Link>
 
-                <button className="btn d-flex align-items-center">
+                <button className="p-0 btn" onClick={ handleLogout }>
                     <FiLogOut className="fs-2 text-warning"/>
                 </button>
             </div>
