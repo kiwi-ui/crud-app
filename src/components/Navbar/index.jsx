07953cb4b6e4e4a9e602a317c0d9a39708bce21d
kiwi-ui <yusnianggara@gmail.com>
2023-08-16 13:19:23 +0700

    refactor: ready for deploy

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
     <header>
         <nav className="navbar z10 border-bottom py-1 bg-dark">
             <div className="container">
-                <Link className="mb-0 bg-warning text-black px-2 rounded-2 fw-bold navbar-brand" to="/users">CrossOver</Link>
+                <Link className="mb-0 bg-warning text-black px-2 rounded-2 fw-bold navbar-brand" to="/users">5Watt</Link>
 
                 <button className="p-0 btn" onClick={ handleLogout }>
                     <FiLogOut className="fs-4 text-warning"/>
