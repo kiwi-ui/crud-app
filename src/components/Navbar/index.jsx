9fd157fd04309d417a659aecb2ab362917102ae7
kiwi-ui <yusnianggara@gmail.com>
2023-08-16 10:12:05 +0700

    style: change icon for logout

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
-import {RiAccountCircleLine} from 'react-icons/ri';
 import { Link } from 'react-router-dom';
+import {FiLogOut} from 'react-icons/fi';
 
 const Navbar = () => {
   return (
     <header>
         <nav className="navbar z10 border-bottom py-1 bg-dark">
             <div className="container">
-                <Link className="mb-0 bg-warning bg-opacity-75 text-black px-2 rounded-2 fw-bold navbar-brand" to="/users">CrossOver</Link>
+                <Link className="mb-0 bg-warning text-black px-2 rounded-2 fw-bold navbar-brand" to="/users">CrossOver</Link>
 
-                <button className="btn d-flex align-items-center gap-2" role="search">
-                    <RiAccountCircleLine className="fs-1 text-warning"/>
+                <button className="btn d-flex align-items-center">
+                    <FiLogOut className="fs-2 text-warning"/>
                 </button>
             </div>
         </nav>
-  </header>
+    </header>
   )
 }
 
